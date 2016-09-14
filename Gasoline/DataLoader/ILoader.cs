172222d570c881d0606fbea67630bb5c2e1614e5
delta.plan <delta.plan473@gmail.com>
2016-09-14 23:46:40 +0500

    Добавил загрузчик данных из Xml

---
+++ Gasoline/DataLoader/ILoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gasoline.Data.Entities;
+
+namespace DataLoader
+{
+    public interface ILoader
+    {
+        void Save(Car car);
+        Car Load();
+    }
+}
---
+++ Gasoline/Gasoline.Core/Entities/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gasoline.Data.Entities
+{
+    [Serializable]
+    public class Car
+    {
+        /// <summary>
+        /// Марка и модель машины
+        /// </summary>
+        public string ModelName { get; set; }
+        /// <summary>
+        /// Краткое описание
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Короткое название машины
+        /// </summary>
+        public string ShortName { get; set; }
+        /// <summary>
+        /// Инвормация о заправках
+        /// </summary>
+        public List<FuelDetail> FuelDetails { get; set; }
+    }
+}
