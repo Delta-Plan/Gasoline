16ec100fa190d26cd92b16edb7ef9efe3caae36b
Ivan Shlykov <delta.plan473@gmail.com>
2023-09-14 14:10:02 +0500

    refactored project structure
    core project
    web api poject
    project for desktop ui

--- Gasoline/Gasolin.Desktop/App.xaml.cs
+++ Gasoline/Gasolin.Desktop/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Gasoline.Desktop
+namespace Gasolin.Desktop
 {
     /// <summary>
     /// Interaction logic for App.xaml
